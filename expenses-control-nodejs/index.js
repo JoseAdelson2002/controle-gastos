930a5b4a7f276f39dfe4a054731317742c22cb73
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2024-12-23 14:20:22 -0300

    conectando o nodejs com express ao banco de dados firestore

--- expenses-control-nodejs/index.js
+++ expenses-control-nodejs/index.js
@@ -1,14 +1,27 @@
-import express from "express";
+import express from 'express';
+import admin from 'firebase-admin'
 
 //REST API http://api.controle-gastos.com/transactions
 const app = express();
 
+admin.initializeApp({
+  credential: admin.credential.cert('serviceAccountKey.json')
+});
+
 //GET       http://api.controle-gastos.com/transactions
 app.get('/transactions', (request, response) => {
     console.log('GET transactions');
-    response.json([{id: 1}]);
+    admin.firestore()
+        .collection('transactions')
+        .get()
+        .then(snapshot => {
+            const transactions = snapshot.docs.map(doc => ({
+                ...doc.data(),
+                uid: doc.id
+            }))
+            response.json(transactions);
+        })
 })
-
 
 
 app.listen(3000, () => console.log('API REST iniciada em http://localhost:3000'));
