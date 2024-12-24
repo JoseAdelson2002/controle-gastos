43db1f3710a0253c2ba7d2fe4ccc515c552987b9
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2024-12-24 15:05:45 -0300

    Valida dados de entrada da criação de uma transação com middleware

--- expenses-control-nodejs/transactions/routes.js
+++ expenses-control-nodejs/transactions/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import admin from 'firebase-admin';
 import { authenticateToken } from '../middlewares/authenticate-jwt.js';
 import { TransactionController } from './controller.js';
+import { validateCreateTransaction } from './validators/create-transaction.validator.js';
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get('/:uid',
 );
 
 app.post('/',
+    (request, response, next) => validateCreateTransaction(request, response, next),
     (request, response, next) => authenticateToken(request, response, next, admin.auth()),
     (request, response) => transactionController.create(request, response)
 
