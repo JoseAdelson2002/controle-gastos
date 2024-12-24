8190bd6d94d7699b60a289448cb0ae3d197f3aca
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2024-12-24 17:42:58 -0300

    corrige problemas de frontend

--- expenses-control-html-css-js/index.js
+++ expenses-control-html-css-js/index.js
@@ -1,53 +1,116 @@
-import { auth } from './firebase-init.js';
-import { onAuthStateChanged, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
-
-// Formulário e seus elementos
 const form = {
     email: () => document.getElementById('email'),
     emailInvalidError: () => document.getElementById('email-invalid-error'),
     emailRequiredError: () => document.getElementById('email-required-error'),
-    loginButton: () => document.getElementById('login-button'),
+    loginButton: document.getElementById('login-button'),
     password: () => document.getElementById('password'),
     passwordRequiredError: () => document.getElementById('password-required-error'),
     recoverPassword: () => document.getElementById('recover-password-button')
 };
 
-// Observa mudanças no estado do usuário
-onAuthStateChanged(auth, user => {
+firebase.auth().onAuthStateChanged(user => {
     if (user) {
         window.location.href = "pages/home/home.html";
     }
-});
+})
 
-// Função de login
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+}
+
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
 function login() {
     showLoading();
-    const email = form.email().value;
-    const password = form.password().value;
-
-    signInWithEmailAndPassword(auth, email, password)
-        .then(() => {
-            hideLoading();
-            window.location.href = "pages/home/home.html";
-        })
-        .catch(error => {
-            hideLoading();
-            alert(getErrorMessage(error));
-        });
+    firebase.auth().signInWithEmailAndPassword(form.email().value, form.password().value).then(response => {
+        hideLoading();
+        window.location.href = "pages/home/home.html";
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
 }
 
-// Função para recuperar senha
+function getErrorMessage(error) {
+    if (error.code == "auth/invalid-credential") {
+        return "Usuário não encontrado";
+    }
+    if (error.code == "auth/wrong-password") {
+        return "Senha inválida";
+    }
+    return error.message;
+}
+
+function register() {
+    window.location.href = "pages/register/register.html";
+}
+
 function recoverPassword() {
     showLoading();
-    const email = form.email().value;
-
-    sendPasswordResetEmail(auth, email)
-        .then(() => {
-            hideLoading();
-            alert('Email enviado com sucesso!');
-        })
-        .catch(error => {
-            hideLoading();
-            alert(getErrorMessage(error));
-        });
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert('Email enviado com sucesso!');
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
 }
+
+function isEmailValid() {
+    const email = form.email().value.trim();
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function isPasswordValid() {
+    const password = form.password().value.trim();
+    return password !== ""; 
+}
+
+function toggleEmailErrors() {
+    const email = form.email().value.trim();
+    form.emailRequiredError().style.display = email ? "none" : "block"; 
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block"; 
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value.trim();
+    form.passwordRequiredError().style.display = password ? "none" : "block"; 
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    const passwordValid = isPasswordValid();
+
+    // Esqueceu a senha
+    const recoverButton = form.recoverPassword();
+    if (!emailValid) {
+        recoverButton.setAttribute("disabled", "true");
+    } else {
+        recoverButton.removeAttribute("disabled");
+    }
+    
+    // Entrar
+    const loginButton = form.loginButton;
+    if (emailValid && passwordValid) {
+        loginButton.removeAttribute("disabled");
+    } else {
+        loginButton.setAttribute("disabled", "true");
+    }
+}
+
+document.getElementById('email').addEventListener('input', () => {
+    toggleButtonsDisable();
+    toggleEmailErrors();
+});
+
+document.getElementById('password').addEventListener('input', () => {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+});
